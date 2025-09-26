de46b280605ff235c4a6a05eaed28530ec289943
Kevin Hernandez-Nino <klhn2312@gmail.com>
2025-09-25 22:09:02 -0700

    Initial Commit

---
+++ spotter./spotter_App.swift
@@ -0,0 +1,21 @@
+//
+//  spotter_App.swift
+//  spotter.
+//
+//  Created by Kevin Hernandez-Nino on 9/25/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct spotter_App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ spotter.Tests/spotter_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  spotter_Tests.swift
+//  spotter.Tests
+//
+//  Created by Kevin Hernandez-Nino on 9/25/25.
+//
+
+import Testing
+@testable import spotter_
+
+struct spotter_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
